31d7dc4341ec7ed679be4b7dc5e27af7588e8ad6
Roberto <robertomendonca20@gmail.com>
2019-11-07 14:01:19 -0300

    feat: add hover icons header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { MdNotificationsNone, MdMoreVert } from 'react-icons/md';
 
-import { Container, Notification } from './styles';
+import { Container, Notification, Badge, Profile } from './styles';
 
 export default function Header() {
   return (
@@ -9,9 +9,13 @@ export default function Header() {
       <h1>Rocketfy</h1>
 
       <Notification>
-        <MdNotificationsNone size={28} color="rgba(255, 255, 255, 0.6)" />
-        <img src="https://avatars0.githubusercontent.com/u/20547884?v=4" alt="avatar"/>
-        <MdMoreVert size={28} color="rgba(255, 255, 255, 0.6)" />
+        <Badge>
+          <MdNotificationsNone size={28} color="rgba(255, 255, 255, 0.7)" />
+        </Badge>
+        <Profile>
+          <img src="https://avatars0.githubusercontent.com/u/20547884?v=4" alt="avatar"/>
+          <MdMoreVert size={28} color="rgba(255, 255, 255, 0.6)" />
+        </Profile>
       </Notification>
     </Container>
   );
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -9,18 +9,38 @@ export const Container = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
-
-
 `;
 
 export const Notification = styled.div`
   display: flex;
   align-items: center;
+`;
+
+export const Badge = styled.button`
+  background: none;
+  border: 0;
+  margin-right: 5px;
+  transition: opacity 0.2s;
+
+  &:hover {
+    opacity: 0.7;
+  }
+`;
+
+export const Profile = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 7px;
+  transition: background 0.2s;
+
+  &:hover {
+    background: rgba(255, 255, 255, 0.2);
+    border-radius: 4px;
+  }
 
   img {
     width: 32px;
     height: 32px;
     border-radius: 4px;
-    margin: 15px;
   }
 `;
